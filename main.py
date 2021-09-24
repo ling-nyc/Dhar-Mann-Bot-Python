7177dc8b0d84f47861af58840dd0476c16d3a68c
Ling <39093500+ling-nyc@users.noreply.github.com>
2021-09-23 19:41:00 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import discord
+import requests
+import json
+import random
+from discord.ext import commands
+a
+ctivity = discord.Activity(name=f' Dhar Man', type=3)
+# Set this to any status the bot should have on start
+# Types:
+#   1 - Playing ...
+#   2 - Listening to ...
+#   3 - Watching ...
+#   4 - Streaming (you can set url='') ...
+#   5 - Competing in ...
+client = discord.Client()
+
+token = os.environ['BOT_TOKEN']
+
+
+with open('words.json') as f:
+  words = json.load(f)
+
+# words['people'], words['actions'], words['endings']
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=(activity))
+    print('Bot is ready.')
+
+@client.event 
+async def on_message(message):
+
+  if not message.author.bot:
+
+    if message.content.startswith('dhar man'):
+    
+      title = f"{random.choice(words['people'])} {random.choice(words['actions'])} {random.choice(words['people'])}, {random.choice(words['endings'])}!"
+      
+      await message.reply(title)
+    if message.content.startswith('dhar ping'):
+
+      await ctx.reply('Pong! %0.2fms' % client.latency)
+
+client.run(token)
+
+
